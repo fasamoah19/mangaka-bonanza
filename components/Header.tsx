eaf9fb5f1c8c8bb2e9e5be90747dcec6591b6b5b
Frederick Ankamah Asamoah Jr <fasamoah@wsinnovate.com>
2023-04-19 12:40:17 -0400

    Removing old_home page Adding a custom UserIcon component Cleaning up the Header and index file that contained chakra-ui code

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
-import { Avatar, Flex, Heading, Link, Spacer } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/react";
 import { Anton } from "next/font/google";
+import UserIcon from "./icons/UserIcon";
 
 /** Font used for the logo */
 const anton = Anton({ subsets: ["latin"], weight: "400" });
@@ -10,33 +11,6 @@ const anton = Anton({ subsets: ["latin"], weight: "400" });
  * @returns Header componenet
  */
 export default function Header() {
-  return (
-    <>
-      {/* <HeaderChakraUI /> */}
-      <HeaderTailwindCSS />
-    </>
-  );
-}
-
-const HeaderChakraUI = () => {
-  return (
-    <Flex as="header">
-      <Heading as={"h3"} fontSize={"3xl"} fontFamily={anton.style.fontFamily}>
-        Mangaka Bonanza
-      </Heading>
-      <Spacer />
-      <Flex as="nav" gap={4} fontSize={14} align={"center"}>
-        <Link>Home</Link>
-        <Link>Genres</Link>
-        <Link>Cart</Link>
-        <Link>About</Link>
-        <Avatar size={"sm"} name="Frederick Asamoah" bg="red.300" />
-      </Flex>
-    </Flex>
-  );
-};
-
-const HeaderTailwindCSS = () => {
   return (
     <header className="flex sticky z-50 top-0 bg-white py-4">
       <Link href="/">
@@ -67,21 +41,8 @@ const HeaderTailwindCSS = () => {
           </li>
         </ul>
 
-        <div className="relative w-8 h-8 overflow-hidden bg-red-300 rounded-full dark:bg-gray-600">
-          <svg
-            className="absolute w-10 h-10 text-gray-400 -left-1"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </div>
+        <UserIcon />
       </nav>
     </header>
   );
-};
+}
