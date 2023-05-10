05b5acbf2c2c6860948ea383a96430ffa0fd23bc
Frederick Ankamah Asamoah Jr <fasamoah@wsinnovate.com>
2023-05-10 09:19:01 -0400

    Fixing header menu Adding genres dropdown submenu

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,8 @@ import { useCartContext } from "@/context/CartContextProvider";
 import Link from "next/link";
 import { genres } from "@/lib/constants";
 import HamburgerMenu from "./icons/HamburgerMenu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useFetchScrollY from "@/hooks/useFetchScrollY";
 
 /**
  * Sticky Header component
@@ -12,7 +13,26 @@ import { useState } from "react";
  */
 export default function Header() {
   const cart = useCartContext();
-  const [openMenu, setOpenMenu] = useState<boolean>(false);
+  const scrollY = useFetchScrollY();
+  const [showMenu, setShowMenu] = useState<boolean>(false);
+  const [showMobileGenreMenu, setShowMobileGenreMenu] =
+    useState<boolean>(false);
+  const [prevScrollY, setPrevScrollY] = useState<number>(scrollY);
+
+  /** Helper function that closes the menus */
+  const closeMenus = () => {
+    setShowMenu(false);
+    setShowMobileGenreMenu(false);
+  };
+
+  useEffect(() => {
+    // If the user has scrolled 100 pixels up or down from the position where the menu
+    // was opened, we close the menu
+    if (prevScrollY + 100 < scrollY || prevScrollY - 100 > scrollY) {
+      closeMenus();
+      setPrevScrollY(scrollY);
+    }
+  }, [scrollY]);
 
   return (
     <header className="sticky z-50 top-0 bg-white py-4 shadow-sm">
@@ -66,44 +86,65 @@ export default function Header() {
 
           <UserIcon />
         </nav>
+        {/** Mobile Header */}
         <div className="flex md:hidden">
           <div className="group">
-            <HamburgerMenu />
-            <div className="hidden w-36 absolute right-4 px-4 py-4 bg-white shadow-md rounded-xl group-hover:flex">
-              <nav className="flex flex-col space-x-4 place-items-center">
-                <ul className="flex flex-col md:flex-row space-x-6">
-                  <li>
-                    <Link
-                      className="text-sm font-semibold text-gray-600"
-                      href="/"
-                    >
-                      Home
-                    </Link>
-                  </li>
-                  <li className="group">
-                    <Link
-                      className="text-sm font-semibold text-gray-600"
-                      href="#"
-                    >
-                      Genres
-                    </Link>
-                  </li>
-                  <li>
-                    <Link
-                      href="/cart"
-                      className="text-sm font-semibold text-gray-600"
-                    >
-                      Cart
-                    </Link>
-                    {cart.length > 0 ? (
-                      <div className="rounded-full h-2 w-2 bg-siteRed absolute -translate-y-5 translate-x-6"></div>
-                    ) : (
-                      <></>
-                    )}
-                  </li>
-                </ul>
-              </nav>
-            </div>
+            <HamburgerMenu showMenu={showMenu} setShowMenu={setShowMenu} closeMenus={closeMenus} />
+            {/** Dropdown Menu */}
+            {showMenu ? (
+              <div className="hidden w-28 absolute right-4 px-4 py-4 bg-white shadow-md rounded-xl group-hover:flex">
+                <nav className="flex flex-col space-x-4 place-items-center">
+                  <ul className="flex flex-col gap-y-2">
+                    <li onClick={closeMenus}>
+                      <Link
+                        className="text-sm font-semibold text-gray-600"
+                        href="/"
+                      >
+                        Home
+                      </Link>
+                    </li>
+                    <li className="group" onClick={() => {setShowMobileGenreMenu(!showMobileGenreMenu)}}>
+                      <div
+                        className="text-sm font-semibold text-gray-600"
+                      >
+                        Genres
+                      </div>
+                      {/** List of genres in the hidden menu. Menu displays on click/tap of Genres list item */}
+                      {showMobileGenreMenu ? (
+                        <div className="flex flex-col top-10 right-28 w-36 absolute px-4 py-4 bg-white shadow-md rounded-xl gap-y-3">
+                          {genres.map((genre) => (
+                            <Link
+                              href={`/genres/${genre}`}
+                              key={genre}
+                              className="text-sm font-medium hover:text-siteRed text-gray-500"
+                            >
+                              {genre}
+                            </Link>
+                          ))}
+                        </div>
+                      ) : (
+                        <></>
+                      )}
+                    </li>
+                    <li onClick={closeMenus}>
+                      <Link
+                        href="/cart"
+                        className="text-sm font-semibold text-gray-600"
+                      >
+                        Cart
+                      </Link>
+                      {cart.length > 0 ? (
+                        <div className="rounded-full h-2 w-2 bg-siteRed absolute -translate-y-5 translate-x-6"></div>
+                      ) : (
+                        <></>
+                      )}
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            ) : (
+              <></>
+            )}
           </div>
         </div>
       </div>
--- components/icons/HamburgerMenu.tsx
+++ components/icons/HamburgerMenu.tsx
@@ -1,10 +1,30 @@
-export default function HamburgerMenu() {
+import { Dispatch, SetStateAction } from "react";
+
+type HamburgerMenuProps = {
+  showMenu: boolean;
+  setShowMenu: Dispatch<SetStateAction<boolean>>;
+  closeMenus: () => void;
+};
+
+export default function HamburgerMenu({
+  showMenu,
+  setShowMenu,
+  closeMenus,
+}: HamburgerMenuProps) {
   return (
     <svg
       width="20"
       height="16"
       viewBox="0 0 14 10"
       xmlns="http://www.w3.org/2000/svg"
+      onClick={() => {
+        if (showMenu) {
+          closeMenus()
+        }
+        else {
+          setShowMenu(true)
+        }
+      }}
     >
       <line y1="0.5" x2="14" y2="0.5" stroke="black" />
       <line y1="4.18457" x2="14" y2="4.18457" stroke="black" />
